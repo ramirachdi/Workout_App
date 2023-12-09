1a4448985f2f70814dc6315f85f6e6540d59c053
ramirachdi <rachdirami11@gmail.com>
2023-12-09 15:54:21 +0100

    workout

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
+// pages & components
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={ <Home /> }
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
